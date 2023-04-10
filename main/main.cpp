ae00a291c240fac01041245028c1ebf0853d3c9d
Sooraj Karthik <soorajkarthik15@gmail.com>
2023-04-10 18:25:09 -0400

    got nice visualization

--- main/main.cpp
+++ main/main.cpp
@@ -3,7 +3,7 @@
 #include <io.h>
 
 int main(int argc, char *argv[]) {
-    Dataset d(1000, 2, 5);
-    KMeansResult res = d.kmeans_openmp(5, 10, 1e-7);
+    Dataset d(10000, 2, 10);
+    KMeansResult res = d.kmeans_openmp(10, 1000000, 1e-7);
     write_to_file(argv[1], d, res);
 }
